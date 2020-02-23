baa3f15940fb943659fc6dad9495fa3ce4d1c09e
gzhang <tanghebinglin@126.com>
2020-02-23 16:47:47 +0800

    1. 增加dict测试程序

--- src/A_test_dict.c
+++ src/A_test_dict.c
@@ -3,22 +3,30 @@
 
 #define MAX_ITEMS 32
 
+void _serverAssert(char *estr, char *file, int line) {
+
+}
+
+void _serverPanic(const char *file, int line, const char *msg, ...) {
+
+}
+
+
 //--------------- test type
 typedef struct my_str_t {
 	int len;
-	char data[];
+	char *data;
 } my_str_t;
 
 void PrintString(my_str_t *val) {
 	for (int i = 0; i < val->len; i++) {
-		putchar(val[i]);
+		putchar(val->data[i]);
 	}
-	putchar('\n');
 }
 
 // -------------- dict type
 uint64_t HashKey(const void *key) {
-	return (uint64_t) (*(int *)key % MAX_ITEM);
+	return (uint64_t) (*(int *)key % MAX_ITEMS);
 }
 
 void * DupKey(void *privdata, const void *key) {
@@ -33,15 +41,17 @@ void * DupValue(void *privdata, const void *obj) {
 	(void)privdata;
 
 	const my_str_t *orig = (const my_str_t*)obj;
-	if (!db) {
+	if (!orig) {
 		printf("tranfer failed!");
 		return NULL;
 	}
 
-	my_str_t *new_one = (my_str_t *)malloc(my_str_t);
+	my_str_t *new_one = (my_str_t *)malloc(sizeof(my_str_t));
 	new_one->len = orig->len;
 	new_one->data = (char *)malloc(orig->len * sizeof(char));
 	memcpy(new_one->data, orig->data, orig->len);
+
+	return new_one;
 }
 
 int CompareKey(void *privdata, const void *key1, const void *key2) {
@@ -78,35 +88,39 @@ int main(int argc, char *argv[]) {
 	}
 
 	my_str_t string1;
-	string1->data = "hello redis";
-	string1->len = 11;
-	dictAdd(test_dict, 1, &string1);
+	string1.data = "hello redis";
+	string1.len = 11;
+	int idx1 = 1; 
+	dictAdd(test_dict, &idx1, &string1);
 
 	my_str_t string2;
-	string2->data = "hello nginx";
-	string2->len = 11;
-	dictAdd(test_dict, 1, &string2);
+	string2.data = "hello nginx";
+	string2.len = 11;
+	int idx2 = 2; 
+	dictAdd(test_dict, &idx2, &string2);
 
 	my_str_t string3;
-	string3->data = "hello mongodb";
-	string3->len = 13;
-	dictAdd(test_dict, 1, &string3);
+	string3.data = "hello mongodb";
+	string3.len = 13;
+	int idx3 = 3; 
+	dictAdd(test_dict, &idx3, &string3);
 
 	my_str_t string4;
-	string4->data = "hello memecached";
-	string4->len = 16;
-	dictAdd(test_dict, 1, &string4);
+	string4.data = "hello memecached";
+	string4.len = 16;
+	int idx4 = 4;
+	dictAdd(test_dict, &idx4, &string4);
 
 	dictIterator *iter_dict = dictGetIterator(test_dict);
 	dictEntry *dict_node = NULL;
-	while ((dict_node = dictNext(iter_dict)) == NULL) {
+	while ((dict_node = dictNext(iter_dict)) != NULL) {
 		int *key = (int *)dict_node->key;
-		my_str_t *val = (my_str_t *)dict_node->val;
+		my_str_t *val = (my_str_t *)dict_node->v.val;
 		printf("node: key(%d), value(", *key);
 		PrintString(val);
 		printf(") \n");
 	}
-	
+	dictReleaseIterator(iter_dict);
 	dictRelease(test_dict);
 
 	printf("operator over...\n");
